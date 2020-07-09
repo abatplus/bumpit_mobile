34a19aff2feda6dd86ab21e078eae880b3591b04
Harald Schäfer <harald.schaefer@abatplus.de>
2020-07-09 11:47:33 +0200

    - update routing

--- src/pages/MainTabs.tsx
+++ src/pages/MainTabs.tsx
@@ -1,19 +1,13 @@
-import React  from 'react';
+import React from 'react';
 import { IonTabs, IonRouterOutlet, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/react';
 import { Route, Redirect } from 'react-router';
 import { people, qrCode, scan, share } from 'ionicons/icons';
-import VcardView from './VCardView';
+import VCardView from './VCardView';
 import QrView from './QrView';
 import ScanView from './ScanView';
 import SwapView from './SwapView';
 
 const MainTabs: React.FC = () => {
-
-  const swapData = () => {
-    //swapCardData();
-    alert("swapCardData");
-  }
-
   return (
     <IonTabs>
       <IonRouterOutlet>
@@ -22,25 +16,25 @@ const MainTabs: React.FC = () => {
           Using the render method prop cuts down the number of renders your components will have due to route changes.
           Use the component prop when your component depends on the RouterComponentProps passed in automatically.
         */}
-        <Route path="/tabs/vcard" render={() => <VcardView />} exact={true} />
-        <Route path="/tabs/qrcode" render={() => <QrView />} exact={true} />
-        <Route path="/tabs/scan" render={() => <ScanView />} exact={true} />
-        <Route path="/tabs/swap" render={() => <SwapView />} exact={true} />
+        <Route path="/" render={() => <VCardView />} exact={true} />
+        <Route path="/qrcode" render={() => <QrView />} exact={true} />
+        <Route path="/scan" render={() => <ScanView />} exact={true} />
+        <Route path="/swap" render={() => <SwapView />} exact={true} />
       </IonRouterOutlet>
       <IonTabBar slot="bottom">
-        <IonTabButton tab="vcard" href="/tabs/vcard">
+        <IonTabButton tab="vcard" href="/">
           <IonIcon icon={people} />
           <IonLabel>Card</IonLabel>
         </IonTabButton>
-        <IonTabButton tab="qrcode" href="/tabs/qrcode">
+        <IonTabButton tab="qrcode" href="/qrcode">
           <IonIcon icon={qrCode} />
           <IonLabel>QR</IonLabel>
         </IonTabButton>
-        <IonTabButton tab="scan" href="/tabs/scan">
+        <IonTabButton tab="scan" href="/scan">
           <IonIcon icon={scan} />
           <IonLabel>Scan</IonLabel>
         </IonTabButton>
-          <IonTabButton tab="swap" href="/tabs/swap">
+        <IonTabButton tab="swap" href="/swap">
           <IonIcon icon={share} />
           <IonLabel>Swap</IonLabel>
         </IonTabButton>
