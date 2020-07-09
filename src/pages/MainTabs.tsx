202e2cd931d6bd93804b0f000d4099d4ba228924
ajenkuhn <andreas.jenkuhn@manfredmangabatgroup.onmicrosoft.com>
2020-07-09 11:16:32 +0200

    fixed filenames

--- src/pages/MainTabs.tsx
+++ src/pages/MainTabs.tsx
@@ -2,7 +2,7 @@ import React  from 'react';
 import { IonTabs, IonRouterOutlet, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/react';
 import { Route, Redirect } from 'react-router';
 import { people, qrCode, scan, share } from 'ionicons/icons';
-import VcardView from './VCardView';
+import VCardView from './VCardView';
 import QrView from './QrView';
 import ScanView from './ScanView';
 import SwapView from './SwapView';
@@ -22,7 +22,7 @@ const MainTabs: React.FC = () => {
           Using the render method prop cuts down the number of renders your components will have due to route changes.
           Use the component prop when your component depends on the RouterComponentProps passed in automatically.
         */}
-        <Route path="/tabs/vcard" render={() => <VcardView />} exact={true} />
+        <Route path="/tabs/vcard" render={() => <VCardView />} exact={true} />
         <Route path="/tabs/qrcode" render={() => <QrView />} exact={true} />
         <Route path="/tabs/scan" render={() => <ScanView />} exact={true} />
         <Route path="/tabs/swap" render={() => <SwapView />} exact={true} />
