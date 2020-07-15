f346459c056f970d2bc7e173a8039087d8254402
ABATGROUP\ovela <ovi.vela@abatplus.de>
2020-07-15 08:09:21 +0200

    menu new version

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -3,14 +3,11 @@ import { IonReactRouter } from '@ionic/react-router';
 import { IonSplitPane, IonRouterOutlet } from '@ionic/react';
 import { Route } from 'react-router';
 import MainTabs from '../pages/MainTabs';
-import About from '../pages/About';
-import Privacy from '../pages/Privacy';
-import LegalInfo from '../pages/LegalInfo';
 import { useVCard } from '../store/contexts/VCardContext';
 import { getAppData } from '../store/dataApi';
 import * as Actions from '../store/actions/actions';
 import Menu from './Menu';
-import VCardView from '../pages/VCardView';
+
 
 const AppRouter: React.FC = () => {
   const { dispatchVCard } = useVCard();
@@ -28,10 +25,6 @@ const AppRouter: React.FC = () => {
         <Menu />
         <IonRouterOutlet id="main">
           <Route path="/" component={MainTabs} />
-          <Route path="/vcard" component={VCardView} />
-          <Route path="/about" component={About} />
-          <Route path="/privacy" component={Privacy} />
-          <Route path="/legal" component={LegalInfo} />
         </IonRouterOutlet>
       </IonSplitPane>
     </IonReactRouter>
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { withRouter, useLocation } from 'react-router';
-import { IonContent, IonIcon, IonItem, IonLabel, IonList, IonListHeader, IonMenu, IonMenuToggle } from '@ionic/react';
-import { peopleCircleOutline } from 'ionicons/icons';
+import { IonContent, IonIcon, IonItem, IonLabel, IonList, IonMenu, IonMenuToggle, IonFooter, IonToolbar, IonTitle, IonHeader, IonImg } from '@ionic/react';
+import { peopleOutline, scan } from 'ionicons/icons';
 import { useAppContext } from '../store/contexts/AppContext';
 import './Menu.css';
 import { useIntl } from 'react-intl';
 import IvCardTranslations from '../i18n/IvCardTranslations';
 import { nameof } from '../utils';
+
 
 interface Pages {
   title: string;
@@ -22,6 +23,11 @@ const Menu: React.FC = () => {
   const i18n = useIntl();
 
   const routes = {
+    tabsPages: [
+      { title: i18n.formatMessage({ id: nameof<IvCardTranslations>('Card') }), path: '/vcard', icon: peopleOutline },
+      { title: i18n.formatMessage({ id: nameof<IvCardTranslations>('Scan') }), path: '/scan', icon: scan }
+
+    ],
     appPages: [
       { title: i18n.formatMessage({ id: nameof<IvCardTranslations>('Terms_and_conditions') }), path: 'termsAndConditions' },
       { title: i18n.formatMessage({ id: nameof<IvCardTranslations>('Privacy_Protection') }), path: '/privacy' },
@@ -42,7 +48,7 @@ const Menu: React.FC = () => {
             className={location.pathname.startsWith(p.path) ? 'selected' : undefined}
             disabled={appContext.isLoading}
           >
-            {/* <IonIcon slot="start" icon={p.icon} /> */}
+            {p.icon && <IonIcon slot="start" icon={p.icon} />}
             <IonLabel>{p.title}</IonLabel>
           </IonItem>
         </IonMenuToggle>
@@ -53,15 +59,28 @@ const Menu: React.FC = () => {
     <IonMenu type="overlay" disabled={!appContext.menuEnabled} contentId="main">
       <IonContent forceOverscroll={false}>
         <IonList lines="none">
-          <IonListHeader>
-            <IonItem>
-              <IonIcon slot="end" icon={peopleCircleOutline} />
-              <IonLabel>{i18n.formatMessage({ id: nameof<IvCardTranslations>('appName') })}</IonLabel>
-            </IonItem>
-          </IonListHeader>
+          <IonHeader>
+            <IonToolbar>
+              <IonItem>
+                <IonImg  className="app-icon" src = "../../assets/icon/Logo_dummy.png"></IonImg>
+              </IonItem>
+              <IonItem>
+                <IonLabel className="app-name">{i18n.formatMessage({ id: nameof<IvCardTranslations>('appName') })}</IonLabel>
+              </IonItem>
+            </IonToolbar>
+          </IonHeader>
+          {renderlistItems(routes.tabsPages)}
+        </IonList>
+        <IonHeader color="primary" />
+        <IonList lines="none">
           {renderlistItems(routes.appPages)}
         </IonList>
       </IonContent>
+      <IonFooter className="ion-no-border">
+        <IonToolbar>
+          <IonTitle color="medium" size="small">powered by abat+</IonTitle>
+        </IonToolbar>
+      </IonFooter>
     </IonMenu>
   );
 };
