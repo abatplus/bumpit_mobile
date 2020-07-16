005fa02234035c3c62899f4ff2dd2911b73984e0
ovivela <56580672+ovivela@users.noreply.github.com>
2020-07-16 08:17:00 +0200

    Bump 44 add profile (#24)
    * add profile
    
    * add new profile
    
    * remove some labels
    
    * some small changes
    
    Co-authored-by: ABATGROUP\ovela <ovi.vela@abatplus.de>

--- src/pages/VCardProfiles.tsx
+++ src/pages/VCardProfiles.tsx
@@ -1,5 +1,18 @@
 import React, { useEffect } from 'react';
-import { IonPage, IonHeader, IonToolbar, IonButtons, IonBackButton, IonTitle, IonContent, IonFab, IonFabButton, IonIcon, IonMenuButton } from '@ionic/react';
+import { useHistory } from 'react-router';
+import {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonBackButton,
+    IonTitle,
+    IonContent,
+    IonFab,
+    IonFabButton,
+    IonIcon,
+    IonMenuButton,
+} from '@ionic/react';
 import { useIntl } from 'react-intl';
 import { nameof } from '../utils';
 import IvCardTranslations from '../i18n/IvCardTranslations';
@@ -9,52 +22,64 @@ import { addCircle } from 'ionicons/icons';
 import ProfileCard from '../components/ProfileCard';
 import * as Actions from '../store/actions/actions';
 import { useAppContext } from '../store/contexts/AppContext';
-import { getProfileData } from '../store/dataApi';
+import { getProfileData, getEmptyProfile } from '../store/dataApi';
+import { v4 as uuidv4 } from 'uuid';
 
 const renderProfiles = (profiles: IProfile[]) => {
-  return profiles.map((profile: IProfile) => <ProfileCard profile={profile} key={profile.id} />);
+    return profiles.map((profile: IProfile) => <ProfileCard profile={profile} key={profile.id} />);
 };
 
 const VCardProfiles: React.FC = () => {
-  const i18n = useIntl();
-  const { profileContext, dispatchProfileContext } = useProfileContext();
-  const { dispatchAppContext } = useAppContext();
+    const history = useHistory();
+    const i18n = useIntl();
+    const { profileContext, dispatchProfileContext } = useProfileContext();
+    const { dispatchAppContext } = useAppContext();
 
-  useEffect(() => {
-    dispatchAppContext(Actions.App.setLoading(true));
+    useEffect(() => {
+        dispatchAppContext(Actions.App.setLoading(true));
 
-    const getProfiles = (async () => await getProfileData())();
-    getProfiles.then((res) => dispatchProfileContext(Actions.Profile.setProfiles(res)));
-    setTimeout(() => {
-      dispatchAppContext(Actions.App.setLoading(false));
-    }, 2000);
-  }, [dispatchAppContext, dispatchProfileContext]);
+        const getProfiles = (async () => await getProfileData())();
+        getProfiles.then((res) => dispatchProfileContext(Actions.Profile.setProfiles(res)));
+        setTimeout(() => {
+            dispatchAppContext(Actions.App.setLoading(false));
+        }, 2000);
+    }, [dispatchAppContext, dispatchProfileContext]);
 
-  return (
-    <IonPage>
-      <IonHeader translucent={true}>
-        <IonToolbar>
-          <IonButtons slot="start">
-            <IonMenuButton />
-            <IonBackButton />
-          </IonButtons>
-          <IonTitle>{i18n.formatMessage({ id: nameof<IvCardTranslations>('Profiles') })}</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent slot={'fixed'}>
-        {profileContext.profiles && profileContext.profiles.length > 0 ? <div>{renderProfiles(profileContext.profiles)}</div> : <React.Fragment />}
-        <IonFab vertical={'bottom'} horizontal={'end'} slot={'fixed'}>
-          <IonFabButton
-            onClick={() => {
-              console.log('Add a new profile');
-            }}
-          >
-            <IonIcon icon={addCircle} />
-          </IonFabButton>
-        </IonFab>
-      </IonContent>
-    </IonPage>
-  );
+    function addNewProfile() {
+        let newProfile: IProfile = getEmptyProfile(uuidv4());
+        newProfile.name = i18n.formatMessage({ id: nameof<IvCardTranslations>('Set_Profile_Name') });
+        dispatchProfileContext(Actions.Profile.addNewProfile(newProfile));
+        history.push('/profile/edit/' + newProfile.id);
+    }
+
+    return (
+        <IonPage>
+            <IonHeader translucent={true}>
+                <IonToolbar>
+                    <IonButtons slot='start'>
+                        <IonMenuButton />
+                        <IonBackButton />
+                    </IonButtons>
+                    <IonTitle>{i18n.formatMessage({ id: nameof<IvCardTranslations>('Profiles') })}</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent slot={'fixed'}>
+                {profileContext.profiles && profileContext.profiles.length > 0 ? (
+                    <div>{renderProfiles(profileContext.profiles)}</div>
+                ) : (
+                    <React.Fragment />
+                )}
+                <IonFab vertical={'bottom'} horizontal={'end'} slot={'fixed'}>
+                    <IonFabButton
+                        onClick={() => {
+                            addNewProfile();
+                        }}>
+                        <IonIcon icon={addCircle} />
+                    </IonFabButton>
+                </IonFab>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default VCardProfiles;
