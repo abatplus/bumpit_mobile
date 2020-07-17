0fdc55aa9df7df24caaef79088fe827e8607f7e2
Michael Theobald <michael.theobald@abatplus.de>
2020-07-17 15:09:15 +0200

    added local server connection

--- src/Server/CardExchangeHub.ts
+++ src/Server/CardExchangeHub.ts
@@ -2,14 +2,14 @@ import * as signalR from '@microsoft/signalr';
 import { ICardExchangeHub } from './ICardExchangeHub';
 
 export enum CardExchangeHubMethod {
-  Subscribe = 'subscribe',
-  Unsubcribe = 'unsubcribe',
-  Update = 'update',
+  Subscribe = 'Subscribe',
+  Unsubcribe = 'Unsubcribe',
+  Update = 'Update',
 
-  RequestCardExchange = 'requestCardExchange',
-  RevokeCardExchangeRequest = 'revokeCardExchangeRequest',
-  AcceptCardExchange = 'acceptCardExchange',
-  SendCardData = 'sendCardData',
+  RequestCardExchange = 'RequestCardExchange',
+  RevokeCardExchangeRequest = 'RevokeCardExchangeRequest',
+  AcceptCardExchange = 'AcceptCardExchange',
+  SendCardData = 'SendCardData',
 }
 
 export class CardExchangeHub implements ICardExchangeHub {
@@ -19,26 +19,35 @@ export class CardExchangeHub implements ICardExchangeHub {
     this.connection = connection;
   }
 
+  transaction = async (func: () => Promise<void>) => {
+    if (this.connection.state !== signalR.HubConnectionState.Connected) {
+      await this.connection.start().then(func);
+    }
+    else {
+      await func();
+    }
+  }
+
   public Subscribe = async (deviceId: string, longitude: number, latitude: number, displayName: string) => {
-    return await this.connection.send(CardExchangeHubMethod.Subscribe, deviceId, longitude, latitude, displayName);
+    return await this.transaction(() => this.connection.send(CardExchangeHubMethod.Subscribe, deviceId, longitude, latitude, displayName));
   };
   public Unsubcribe = async (deviceId: string) => {
-    return await this.connection.send(CardExchangeHubMethod.Unsubcribe, deviceId);
+    return await this.transaction(() => this.connection.send(CardExchangeHubMethod.Unsubcribe, deviceId));
   };
   public Update = async (deviceId: string, longitude: number, latitude: number, displayName: string) => {
-    return await this.connection.send(CardExchangeHubMethod.Update, deviceId, longitude, latitude, displayName);
+    return await this.transaction(() => this.connection.send(CardExchangeHubMethod.Update, deviceId, longitude, latitude, displayName));
   };
 
   public RequestCardExchange = async (deviceId: string, peerDeviceId: string, displayName: string) => {
-    return await this.connection.send(CardExchangeHubMethod.RequestCardExchange, deviceId, peerDeviceId, displayName);
+    return await this.transaction(() => this.connection.send(CardExchangeHubMethod.RequestCardExchange, deviceId, peerDeviceId, displayName));
   };
   public RevokeCardExchangeRequest = async (deviceId: string, peerDeviceId: string) => {
-    return await this.connection.send(CardExchangeHubMethod.RevokeCardExchangeRequest, deviceId, peerDeviceId);
+    return await this.transaction(() => this.connection.send(CardExchangeHubMethod.RevokeCardExchangeRequest, deviceId, peerDeviceId));
   };
   public AcceptCardExchange = async (deviceId: string, peerDeviceId: string, peerDisplayName: string, peerCardData: string) => {
-    return await this.connection.send(CardExchangeHubMethod.AcceptCardExchange, deviceId, peerDeviceId, peerDisplayName, peerCardData);
+    return await this.transaction(() => this.connection.send(CardExchangeHubMethod.AcceptCardExchange, deviceId, peerDeviceId, peerDisplayName, peerCardData));
   };
   public SendCardData = async (deviceId: string, peerDeviceId: string, displayName: string, cardData: string) => {
-    return await this.connection.send(CardExchangeHubMethod.SendCardData, deviceId, peerDeviceId, displayName, cardData);
+    return await this.transaction(() => this.connection.send(CardExchangeHubMethod.SendCardData, deviceId, peerDeviceId, displayName, cardData));
   };
 }
