ca5e2a7bbe32978bd6d52dc4f7150b09c904d386
Michael Theobald <michael.theobald@abatplus.de>
2020-07-20 09:53:14 +0200

    added full test to server

--- src/Server/Tests/CardExchangeServerTest.ts
+++ src/Server/Tests/CardExchangeServerTest.ts
@@ -1,10 +1,15 @@
-import { MockCompleteServer } from './MockCompleteServer';
-import { CardExchangeEvent } from '../CardExchangeEvents';
 import { MockCardExchangeClient } from './MockCardExchangeClient';
 import CardExchangeServer from '../CardExchangeServer';
 
 export class CardExchangeServerTest {
   constructor() {
+    // this.testServer();
+    this.testMultipleInstances();
+  }
+
+  sleep = (seconds: number) => new Promise(resolve => setTimeout(resolve, seconds * 1000));
+
+  testServer = () => {
     const server = new CardExchangeServer(new MockCardExchangeClient());
 
     server.Hub.Subscribe('1001', 1, 0, 'Optimus Prime')
@@ -14,5 +19,24 @@ export class CardExchangeServerTest {
       .then(_ => server.Hub.RevokeCardExchangeRequest('123', '1001'))
       .then(_ => server.Hub.AcceptCardExchange('123', '1001', 'Jazz', 'DATA'))
       .then(_ => server.Hub.SendCardData('123', '1001', 'Ironhide', 'DATA'));
+  }
+
+  testMultipleInstances = () => {
+    const optimus = new CardExchangeServer(new MockCardExchangeClient('OPTIMUS'));
+    const megatron = new CardExchangeServer(new MockCardExchangeClient('MEGATRON'));
+
+    optimus.Hub.Subscribe('1111', 0, 0, 'Optimus Prime').then(_ => this.sleep(1))
+      .then(_ => megatron.Hub.Subscribe('2222', 0, 0, 'Megatron')).then(_ => this.sleep(1))
+      .then(_ => optimus.Hub.Update('1111', 0.1, 0.1, 'Optimus')).then(_ => this.sleep(1))
+      .then(_ => megatron.Hub.Unsubcribe('2222')).then(_ => this.sleep(1))
+      .then(_ => optimus.Hub.Update('1111', 0.2, 0.2, 'Optimus')).then(_ => this.sleep(1))
+      .then(_ => megatron.Hub.Subscribe('3333', 0, 0, 'Galvatron')).then(_ => this.sleep(1))
+      .then(_ => optimus.Hub.Update('1111', 0.3, 0.3, 'Optimus')).then(_ => this.sleep(1))
+      .then(_ => console.log('END LIFECYCLE / START EXCHANGE')).then(_ => this.sleep(1))
+      .then(_ => optimus.Hub.RequestCardExchange('1111', '3333', 'Optimus Prime(1)')).then(_ => this.sleep(1))
+      .then(_ => optimus.Hub.RevokeCardExchangeRequest('1111', '3333')).then(_ => this.sleep(1))
+      .then(_ => optimus.Hub.RequestCardExchange('1111', '3333', 'Optimus Prime(2)')).then(_ => this.sleep(1))
+      .then(_ => megatron.Hub.AcceptCardExchange('1111', '3333', 'Galvatronus', 'Decepticons')).then(_ => this.sleep(1))
+      .then(_ => optimus.Hub.SendCardData('1111', '3333', 'Optimus Prime', 'Autobots'))
   }
 }
