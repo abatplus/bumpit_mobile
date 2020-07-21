10dbc54138a4bc029e82df90039440aa00e3811c
Harald Schäfer <harald.schaefer@abatplus.de>
2020-07-21 14:03:45 +0200

    - removed not needed spreadoperators because new contact

--- src/contacts/ContactApi.ts
+++ src/contacts/ContactApi.ts
@@ -18,11 +18,12 @@ export default class ContactApi {
       contact.name = new ContactName(undefined, vCardFields.name);
       contact.displayName = vCardFields.name;
     }
+
     if (vCardFields.company) {
       contact.organizations = [new ContactOrganization('work', vCardFields.company, undefined, vCardFields.position)];
     }
     if (vCardFields.email) {
-      contact.emails = [...contact.emails, new ContactField('work', vCardFields.email)];
+      contact.emails = [new ContactField('work', vCardFields.email)];
     }
     if (vCardFields.phone) {
       contact.phoneNumbers = [new ContactField('mobile', vCardFields.phone)];
@@ -31,16 +32,13 @@ export default class ContactApi {
       contact.phoneNumbers = [...contact.phoneNumbers, new ContactField('work', vCardFields.fax)];
     }
     if (vCardFields.website) {
-      contact.urls = [...contact.urls, new ContactField('work', vCardFields.website)];
+      contact.urls = [new ContactField('work', vCardFields.website)];
     }
     contact.addresses = [
-      ...contact.addresses,
       new ContactAddress(false, 'work', undefined, vCardFields.street, vCardFields.location, undefined, vCardFields.zipCode, vCardFields.country),
     ];
 
     contact.note = 'Added by vSwap';
-    contact.categories = [...contact.categories, new ContactField('work', 'vSwap Exchanged')];
-
     return contact.save();
   }
 }
