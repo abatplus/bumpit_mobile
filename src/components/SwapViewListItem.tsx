eb10d136ffb70372a39536d9e7ca4d28f32d4dff
ajenkuhn <andreas.jenkuhn@abatplus.de>
2020-08-12 11:14:26 +0200

    removed debug alert

--- src/components/SwapViewListItem.tsx
+++ src/components/SwapViewListItem.tsx
@@ -79,11 +79,9 @@ const SwapViewListItem = ({
 
     const renderAvatarImage = () => {
         if (thumbnailUrl) {
-            alert(thumbnailUrl);
             const imageClass = state === SwapState.requested ? 'swap-list-image-shadowed' : 'swap-list-image';
             return <img src={thumbnailUrl} alt={name} className={imageClass} />;
         } else {
-            alert('no-thumb');
             return <FontAwesomeIcon className='fa fa-lg swap-list-no-user' icon={faUser} />;
         }
     };
